328ca6991b7fed7e6ccf9c1e689fbed85ae31939
EnzoZardo <enzo.zardo@gmail.com>
2026-03-30 20:22:44 -0300

    refactor: fazendo if com 0 usando not (coisa errada)

--- board.py
+++ board.py
@@ -21,7 +21,7 @@ class Board:
         print('    0   1   2\n  +---+---+---+');
         for i, row in enumerate(self.board):
             for j, cell in enumerate(row):
-                if (j == 0): print(i, end=' ')
+                if not j: print(i, end=' ')
                 print('|', constants.map[cell], end=' ');
             print('|');
             print('  +---+---+---+');
